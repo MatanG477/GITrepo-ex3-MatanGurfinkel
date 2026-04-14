bc60b87df177230c3c0d8d395e2df801a9d16141
MatanG477 <matan268@gmail.com>
2026-04-14 11:24:18 +0300

    Initial commit for Exercise MTA

---
+++ matan.java
@@ -0,0 +1,7 @@
+public class matan {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Matan is working on Jenkins!");
+        }
+    }
+}
